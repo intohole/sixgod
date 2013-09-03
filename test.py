68b1ab38bcef78066c1c8740fdba4a585e069a14
lixuze <lixuze@iyuewe.com>
2013-09-04 00:54:15 +0800

    添加测试 ---》 要实现基于行块分布函数的正文提取

--- test.py
+++ test.py
@@ -6,9 +6,18 @@
 import util
 
 
+import re
+# python 正则 . 除换行符任意一个字符
+clear_entre = re.compile("\n")
+script_regx = re.compile("<script.*?>.*?</script>",re.IGNORECASE)
+dto_regx = re.compile("<!DOCTYPE.*?>")
+comment_regx = re.compile("<!--.*?-->")
+style_regx = re.compile("<style.*?>.*?</style>")
 
-html = util.get_url_data("http://www.chinanews.com/fz/2013/08-28/5213451.shtml")
 
-for h in html.split("\n"):
+html = util.get_url_data("http://news.gmw.cn/newspaper/2013-08/21/content_1987269.htm",codemode='utf-8')
+x = clear_entre.sub("",html)
+x = script_regx.sub("",x)
+for h in x.split("\r"):
     print h
     print "******************"
