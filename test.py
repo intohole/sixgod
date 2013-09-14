f884639dcd22b0a50ddbb944adf07b67060d798d
lixuze <lixuze@iyuewe.com>
2013-09-15 00:14:28 +0800

    初步测试 继续完成

--- test.py
+++ test.py
@@ -1,23 +1,63 @@
 #coding=utf-8
 
 
-
+#-*- coding:utf-8 -*-
 
 import util
 
 
 import re
+
+class HtmlExtract(object):
+	blocksWidth = 3
+	remove_split = re.compile("\s+")
+	pre_reg_list = [re.compile('(?is)<!DOCTYPE.*?>',re.I),re.compile("(?is)<!--.*?-->",re.I),re.compile('(?is)<script.*?>.*?</script>',re.I),re.compile("(?is)<style.*?>.*?</style>",re.I),re.compile("&.{2,5};|&#.{2,5};",re.I),re.compile('<!--.*?>'),re.compile("(?is)<.*?>",re.I)]
+	# remove_enter = re.compile("\\n\\r")
+	def __init__(self):
+		pass
+
+
+
+	def get_text(self,html):
+		self.lineBlockDistribute([ i for i in self.pre_process(html.encode("utf-8")).split("\n")])
+		
+
+	def lineBlockDistribute(self,lines):
+		indexDistribution = [ len(self.remove_split.sub("",i)) for i in lines]
+		#取出上下两行空行文本
+		for i in range(len(lines)-4):
+			if lines[i] == 0 and lines[i+1] == 0 and lines[i+2] > 0 and lines[i+2] < 40 and lines[i+3] == 0 and lines[i + 4] == 0:
+				lines[i+2] = ""
+				indexDistribution[i+2] = 0
+				i = i+ 1
+				
+		for i in range(len(lines)- self.blocksWidth):
+			wordsum = indexDistribution[i]
+			for j in range(i , i + self.blocksWidth):
+				if j >= len(lines):
+					break
+				wordsum = wordsum + indexDistribution[j]
+			indexDistribution[i] = wordsum
+		return indexDistribution #返回块密度
+
+
+
+
+	def pre_process(self,html):
+		# self.remove_enter.sub("\\r",html) #python do match \n not best ,so i have to replace \r 
+		for reg in self.pre_reg_list:
+			html = reg.sub("",html)
+		return html
+
+
+
+
 # python 正则 . 除换行符任意一个字符
-clear_entre = re.compile("\n")
-script_regx = re.compile("<script.*?>.*?</script>",re.IGNORECASE)
-dto_regx = re.compile("<!DOCTYPE.*?>")
-comment_regx = re.compile("<!--.*?-->")
-style_regx = re.compile("<style.*?>.*?</style>")
 
 
-html = util.get_url_data("http://news.gmw.cn/newspaper/2013-08/21/content_1987269.htm",codemode='utf-8')
-x = clear_entre.sub("",html)
-x = script_regx.sub("",x)
-for h in x.split("\r"):
-    print h
-    print "******************"
+h = HtmlExtract()
+html = util.get_url_data("http://finance.people.com.cn/n/2013/0914/c66323-22919783.html",codemode='gbk')
+h.get_text(html)
+# for h in h.split("\r"):
+#     print h.encode("utf-8")
+#     print "******************"
